9680e88cab05c3b2cc224f4d8796281a71eb6585
Lucas Picault <58218237+lucasPicault@users.noreply.github.com>
2024-12-19 11:31:43 +0100

    vx

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '12db16e75f870e836908fe228cf88f4dfb5b49df',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '12db16e75f870e836908fe228cf88f4dfb5b49df',
+            'dev_requirement' => false,
+        ),
+    ),
+);
