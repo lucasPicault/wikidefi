c9c52ca1d93f448d369785b41670d2db573c98cf
Lucas Picault <58218237+lucasPicault@users.noreply.github.com>
2024-12-19 17:10:33 +0100

    vx

--- wiki-front/public/js/auth.js
+++ wiki-front/public/js/auth.js
@@ -1,11 +1,11 @@
 (async function checkAuthStatus() {
-    const response = await fetch('https://wikidefi.fr/api/index.php/auth/status');
+    const response = await fetch('https://wikidefi.fr/php/index.php/auth/status');
     const data = await response.json();
-    console.log(data);
+    console.log(response);
 
     if (!data.authenticated) {
         console.log('Redirection vers Twitch pour authentification.');
-        window.location.href = 'https://wikidefi.fr/api/index.php/auth/login';
+        window.location.href = 'https://wikidefi.fr/php/index.php/auth/login';
     } else {
         console.log('Utilisateur connecté :', data.user);
     }
