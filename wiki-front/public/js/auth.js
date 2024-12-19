0ba5ce429e66fa2b8ac51f968845c2ee0f77dcef
Lucas Picault <58218237+lucasPicault@users.noreply.github.com>
2024-12-19 17:05:15 +0100

    vx

--- wiki-front/public/js/auth.js
+++ wiki-front/public/js/auth.js
@@ -15,7 +15,7 @@
             console.log('Redirection vers Twitch pour authentification.');
             // Redirigez l'utilisateur directement vers l'URL d'autorisation Twitch
             const clientId = '8x8rp1xpim5kjpywfjvrsrizsxizxi';
-            const redirectUri = encodeURIComponent('https://wikidefi.fr/api.php/auth/callback');
+            const redirectUri = encodeURIComponent('https://wikidefi.fr/api/index.php/auth/callback');
             const scope = encodeURIComponent('user:read:email');
             const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
             window.location.href = twitchAuthUrl;
