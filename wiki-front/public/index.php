a7af9ebd40e27f71ff8ca3e89a1ac45b6cabbe39
Lucas Picault <58218237+lucasPicault@users.noreply.github.com>
2024-12-20 09:51:02 +0100

    vx

--- wiki-front/public/index.php
+++ wiki-front/public/index.php
@@ -9,12 +9,10 @@ $requestUri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 // Charger la page correspondante
 switch ($requestUri) {
     case '':
-        require 'index.php'; // Page d'accueil
-        exit;
+        require 'menu.php'; // Page d'accueil
         break;
     case 'streamer':
         require 'streamer.php'; // Page Streamer
-        exit;
         break;
     case 'viewer':
         require 'viewer.php'; // Page Viewer
@@ -25,35 +23,4 @@ switch ($requestUri) {
         break;
 }
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Accueil</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="/">
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/layout.css">
-    <link rel="stylesheet" href="css/index.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Bienvenue sur le Wiki Game</h1>
-        <div class="buttons">
-            <button id="streamer-btn" role="button">Je suis Streamer</button>
-            <button id="viewer-btn" role="button">Je suis Viewer</button>
-        </div>
-    </div>
-    <script>
-        // Gestion des redirections en utilisant la base URL définie
-        document.getElementById('streamer-btn').addEventListener('click', () => {
-            window.location.href = 'streamer';
-        });
-        document.getElementById('viewer-btn').addEventListener('click', () => {
-            window.location.href = 'viewer';
-        });
-    </script>
-    <script src="js/auth.js"></script>
 
-</body>
-</html>
