729ec3331e602f089ec7b6dd567a06cbbe1c6364
Lucas Picault <58218237+lucasPicault@users.noreply.github.com>
2024-12-28 18:45:51 +0100

    vx

--- wiki-front/public/js/streamer.js
+++ wiki-front/public/js/streamer.js
@@ -11,7 +11,7 @@ document.getElementById('create-session').addEventListener('click', async () => 
   }
 
   try {
-      const response = await fetch('/session/create', {
+      const response = await fetch('https://api.wikidefi.fr/session/create', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
