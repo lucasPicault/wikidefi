e0c289bfb186f5605afdc435d4cc4b262cef1882
Lucas Picault <58218237+lucasPicault@users.noreply.github.com>
2024-12-28 18:54:34 +0100

    vx

--- wiki-front/public/index.php
+++ wiki-front/public/index.php
@@ -2,7 +2,14 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
+header("Access-Control-Allow-Origin: https://wikidefi.fr");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 // Récupérer l'URL demandée
 $requestUri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
